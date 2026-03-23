e1a2e1dfc13b2b5c08665b7c45478c41a04bf8a5
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2026-03-23 10:37:10 -0500

    chore: update biome schema, lint and format

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -4,8 +4,6 @@ import { Pool } from "pg";
 import { env } from "@/lib/config/t3.config";
 import * as schema from "@/lib/db/schema.ts";
 
-const pool = new Pool({
-  connectionString: env.DATABASE_URL,
-});
+const pool = new Pool({ connectionString: env.DATABASE_URL });
 
 export const db = drizzle(pool, { schema });
